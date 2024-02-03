76fc653abcde926268d4902140fc60236a8ff6bd
Andrii Veremii <dashuk10@gmail.com>
2024-02-03 20:06:34 +0100

    add

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -2,9 +2,11 @@ import React from 'react';
 import { Profile } from '../Profile/Profile';
 import { Statistics } from '../Statistics/Statistics';
 import {FriendList} from '../FriendList/FriendList';
+import {TransactionHistory} from '../TransactionHistory/TransactionHistory';
 import user from 'data/user.json';
 import data from 'data/data.json';
 import friends from 'data/friends.json';
+import transactions from 'data/transactions.json'
 import css from './App.module.css';
 
 export const App = () => {
@@ -23,6 +25,8 @@ export const App = () => {
       {/* <Statistics stats={data} /> */}
 
       <FriendList friends={friends}/>
+
+      <TransactionHistory items={transactions} />
     </div>
   );
 };
